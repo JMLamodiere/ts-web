d50a9192ddc09eb626ea64bd7d6fbbef3329d3fa
Jean-Marie Lamodière <j.lamodiere@meetic-corp.com>
2021-09-20 13:48:56 +0200

    feat: add event handler on user model

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ import { User } from "./models/User";
 
 const user = new User({ name: "John", age: 20 });
 
-user.set({ name: "newname" });
+user.on("change", () => {
+  console.log("change #1");
+});
+user.on("change", () => {
+  console.log("change #2");
+});
+user.on("save", () => {
+  console.log("save was triggered");
+});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.trigger("change");
+user.trigger("save");
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,11 @@ interface UserProps {
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  private events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -13,5 +17,22 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback) {
+    const handlers = this.events[eventName] || [];
+    this.events[eventName] = [...handlers, callback];
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
   }
 }
