8a9e6d506f624b2f671a7318350ff2c281159671
Jean-Marie Lamodière <JMLamodiere@users.noreply.github.com>
2021-10-02 22:52:48 +0200

    feat: re-render model on each change

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { UserForm } from "./views/UserForm";
+import { User } from "./models/User";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Element with id root not found");
 }
 
-const userForm = new UserForm(rootElement);
+const user = User.buildUser({ name: "John", age: 20 });
+
+const userForm = new UserForm(rootElement, user);
 
 userForm.render();
